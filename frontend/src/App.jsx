721557246602fc5b902cad555d0f1ecc7caaf113
Suhani1234-5 <suhani.garg@kalvium.commuinity>
2025-02-07 09:29:13 +0530

    milestone12

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,22 +1,20 @@
-import React from "react";
-import { BrowserRouter , Routes , Route} from "react-router-dom";
-import { LoginPage , SignupPage, CreateProduct, Home } from "./Routes.jsx";
-import './App.css' ;
+import React from 'react'
+import { BrowserRouter,Routes,Route } from 'react-router-dom';
+import {LoginPage,SignupPage,Home,CreateProduct, MyProducts} from "./Routes";
+import "./App.css";
 
-
-function App (){
+const App = () => {
   return (
     <BrowserRouter>
-    <Routes>
-    <Route path="/" element={<Home/>} />
-      <Route path="/login" element={<LoginPage />} />
-      <Route path="/signup" element={<SignupPage />} />
-      <Route path="/create-product" element={<CreateProduct/>}/>
-      
-
-    
-    </Routes>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/login' element={<LoginPage/>}/>
+        <Route path="/signup" element={<SignupPage />} />
+        <Route path="/create-product" element={<CreateProduct />} />
+        <Route path="/my-products" element={<MyProducts />} />
+      </Routes>
     </BrowserRouter>
   )
 }
-export default App;
+
+export default App
