654e1678259de2856997fe9fc748bc9528761ec7
Suhani1234-5 <suhanigarg59@gmail.com>
2025-06-28 19:27:00 +0530

    Update

--- backend/app.js
+++ backend/app.js
@@ -5,6 +5,8 @@ const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const product= require('./controller/product')
+const stripeRoute = require('./routes/stripe');
+app.use('/api/v2/stripe', stripeRoute);
 const path=require('path')
 const orders = require('./controller/orders');
 const corsOptions = {
@@ -17,6 +19,7 @@ app.use(cookieParser());
 app.use(cors(corsOptions));
 app.use("/",express.static("uploads"));
 app.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
+
 
 // Configuration for environment variables
 if (process.env.NODE_ENV !== "PRODUCTION") {
@@ -34,5 +37,6 @@ const user = require("./controller/user");
 app.use("/api/v2/user", user);
 app.use("/api/v2/product", product);
 app.use("/api/v2/orders", orders);
+app.use('/api/v2/stripe', stripeRoute);
 app.use(ErrorHandler);
 module.exports= app;
